c276e21072aa9858b1999040e9f395e426cca92f
Brian Lemons <brian_lemons@hotmail.com>
2021-03-22 14:52:24 -0700

    adding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import './App.css';
 
 class App extends React.Component{
@@ -6,7 +6,7 @@ class App extends React.Component{
   render(){
     return(
       <h1>TEST</h1>
-    )
+    );
   }
 }
 
